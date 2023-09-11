84eee0145fcf2cd047be6de6ea474a16874fed42
wang zheng <15237846532@163.com>
2023-09-11 16:04:41 +0800

    first commit

---
+++ eth/eth.h
@@ -0,0 +1,17 @@
+#ifndef __ETH_H__
+#define __ETH_H__
+
+#if __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+void eth_init(void);
+
+#if __cplusplus
+}
+#endif
+
+#endif // !__ETH_H__
---
+++ player/player.h
@@ -0,0 +1,11 @@
+#ifndef __AUDIO_H__
+#define __AUDIO_H__
+
+#include <stdio.h>
+
+
+void player_pwm_init(void);
+
+
+
+#endif // !__AUDIO_H__
