55bd9813a7ae8287c2bb0615ff0b964dcf5231ad
wangzhen <1208827324@qq.com>
2024-10-10 17:03:37 +0800

    rb

--- rb_log/src/rb_log.c
+++ rb_log/src/rb_log.c
@@ -42,15 +42,49 @@ static int _rb_log_vprintf(const char *fmt, va_list args)
 {
     int size = 0;
 #if RB_LOG_BUFF_ENABLE
-    char cache[RB_LOG_CACHE_SIZE] = { 0 };
-    size = vsnprintf(cache, sizeof(cache) - 1, fmt, args);
-    if(s_rb_log && (size > 0) && (size < RB_LOG_CACHE_SIZE)) {
+    int is_heap = 0;
+    char* heap_cache = NULL;
+    char stack_cache[RB_LOG_CACHE_SIZE] = { 0 };
+    int cache_size = RB_LOG_CACHE_SIZE;
+    size = vsnprintf(NULL, 0, fmt, args);
+    if(size > cache_size - 1) {
+        is_heap = 1;
+        cache_size = size + 1;
+    }
+    if(is_heap) {
+#if RB_LOG_BUFF_PSRAM
+        heap_cache = (char*)heap_caps_malloc(cache_size, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+#else
+        heap_cache = (char*)malloc(cache_size);
+#endif // RB_LOG_BUFF_PSRAM
+        if(heap_cache == NULL) {
+#if RB_LOG_DEBUG_ENABLE
+            printf("## rb log heap cache malloc failed!");
+#endif // RB_LOG_DEBUG_ENABLE
+        } else {
+            size = vsprintf(heap_cache, fmt, args);
+        }
+    } else {
+        size = vsprintf(stack_cache, fmt, args);
+    }
+    if(s_rb_log && (size > 0) && (size < cache_size)) {
         if(_rb_log_prepare_buff(size) < 0) {
 #if RB_LOG_DEBUG_ENABLE
-            printf("## log server preare buffer failed!\n");
-#endif
+            printf("## rb log preare buffer failed!\n");
+#endif // RB_LOG_DEBUG_ENABLE
         } else {
-            xRingbufferSend(s_rb_log, cache, size + 1, RB_LOG_BUFF_WR_TIMEOUT);
+            if(is_heap) {
+                if(heap_cache) {
+                    heap_cache[size] = '\0'; // to string
+                    xRingbufferSend(s_rb_log, heap_cache, size + 1, RB_LOG_BUFF_WR_TIMEOUT);
+                }
+            } else {
+                stack_cache[size] = '\0'; // to string
+                xRingbufferSend(s_rb_log, stack_cache, size + 1, RB_LOG_BUFF_WR_TIMEOUT);
+            }
+        }
+        if(is_heap && heap_cache) {
+            free(heap_cache);
         }
     }
 #endif // RB_LOG_BUFF_ENABLE
@@ -64,22 +98,22 @@ int rb_log_init(void)
 {
     int ret = -1;
 #if RB_LOG_BUFF_ENABLE
-    #if RB_LOG_BUFF_PSRAM
-        StaticRingbuffer_t *buff_struct = (StaticRingbuffer_t *)heap_caps_malloc(sizeof(StaticRingbuffer_t), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
-        uint8_t *buff_storage = (uint8_t *)heap_caps_malloc(RB_LOG_BUFF_SIZE, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
-        s_rb_log = xRingbufferCreateStatic(RB_LOG_BUFF_SIZE, RINGBUF_TYPE_NOSPLIT, buff_storage, buff_struct);
-    #else
-        StaticRingbuffer_t *buff_struct = (StaticRingbuffer_t *)malloc(sizeof(StaticRingbuffer_t));
-        uint8_t *buff_storage = (uint8_t *)malloc(RB_LOG_BUFF_SIZE);
-        s_rb_log = xRingbufferCreateStatic(RB_LOG_BUFF_SIZE, RINGBUF_TYPE_NOSPLIT, buff_storage, buff_struct);
-    #endif
+#if RB_LOG_BUFF_PSRAM
+    StaticRingbuffer_t *buff_struct = (StaticRingbuffer_t *)heap_caps_malloc(sizeof(StaticRingbuffer_t), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+    uint8_t *buff_storage = (uint8_t *)heap_caps_malloc(RB_LOG_BUFF_SIZE, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+    s_rb_log = xRingbufferCreateStatic(RB_LOG_BUFF_SIZE, RINGBUF_TYPE_NOSPLIT, buff_storage, buff_struct);
+#else
+    StaticRingbuffer_t *buff_struct = (StaticRingbuffer_t *)malloc(sizeof(StaticRingbuffer_t));
+    uint8_t *buff_storage = (uint8_t *)malloc(RB_LOG_BUFF_SIZE);
+    s_rb_log = xRingbufferCreateStatic(RB_LOG_BUFF_SIZE, RINGBUF_TYPE_NOSPLIT, buff_storage, buff_struct);
+#endif // RB_LOG_BUFF_PSRAM
     if(s_rb_log == NULL) {
-        printf("## log server ring buffer create failed!");
+        printf("## rb log ring buffer create failed!");
     }
-#endif
+#endif // RB_LOG_BUFF_ENABLE
 #if RB_LOG_BUFF_ENABLE || RB_LOG_UART_ENABLE
     esp_log_set_vprintf(_rb_log_vprintf);
-#endif
+#endif // RB_LOG_BUFF_ENABLE || RB_LOG_UART_ENABLE
     ret = 0;
     return ret;
 }
@@ -103,12 +137,12 @@ char* rb_log_get_msg(void)
     vRingbufferGetInfo(s_rb_log, NULL, NULL, NULL, NULL, &msg_num);
     if(msg_num == 0) {
 #if RB_LOG_DEBUG_ENABLE
-        printf("## log server message is empty!\n");
-#endif
+        printf("## rb log message is empty!\n");
+#endif // RB_LOG_DEBUG_ENABLE
         return msg;
     }
     msg = (char*)xRingbufferReceive(s_rb_log, &msg_len, RB_LOG_BUFF_RD_TIMEOUT);
-#endif
+#endif // RB_LOG_BUFF_ENABLE
     return msg;
 }
 
@@ -121,7 +155,7 @@ int rb_log_free_msg(char* msg)
         return ret;
     }
     vRingbufferReturnItem(s_rb_log, (void*)msg);
-#endif
+#endif // RB_LOG_BUFF_ENABLE
     return ret;
 }
 
