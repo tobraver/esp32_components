632ea63e70b1ba23580fcedda3f4c5b3bf10cd63
wangzhen <1208827324@qq.com>
2024-12-03 15:43:09 +0800

    sntp组件

---
+++ utils_sntp/include/utils_sntp.h
@@ -0,0 +1,8 @@
+#ifndef __SNTP_H__
+#define __SNTP_H__
+
+#include "stdio.h"
+
+void utils_sntp_init(void);
+
+#endif // __SNTP_H__
---
+++ utils_sntp/src/utils_sntp.c
@@ -0,0 +1,53 @@
+#include "sntp.h"
+#include "esp_sntp.h"
+#include "esp_log.h"
+#include "esp_err.h"
+
+static const char* TAG = "sntp";
+
+void time_sync_notification_cb(struct timeval *tv)
+{
+    ESP_LOGI(TAG, "Notification of a time synchronization event");
+}
+
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    esp_sntp_setoperatingmode(ESP_SNTP_OPMODE_POLL);
+    esp_sntp_setservername(0, "pool.ntp.org");
+    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+    sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
+#endif
+    esp_sntp_init();
+}
+
+static void obtain_time(void)
+{
+    /**
+     * NTP server address could be aquired via DHCP,
+     * see LWIP_DHCP_GET_NTP_SRV menuconfig option
+     */
+#if LWIP_DHCP_GET_NTP_SRV
+    esp_sntp_servermode_dhcp(1);      // accept NTP offers from DHCP server, if any
+#endif
+
+    initialize_sntp();
+
+    // wait for time to be set
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 10;
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+    time(&now);
+    localtime_r(&now, &timeinfo);
+}
+
+void utils_sntp_init(void)
+{
+    obtain_time();
+}
