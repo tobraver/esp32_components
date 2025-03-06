095910f6820425b5d36cc7f904f6de369c353d8f
wangzhen <1208827324@qq.com>
2025-03-06 14:05:23 +0800

    eth update

--- eth/eth.c
+++ eth/eth.c
@@ -13,6 +13,8 @@
 #endif // CONFIG_ETH_USE_SPI_ETHERNET
 
 static const char *TAG = "ETH";
+static esp_netif_t *g_eth_netif = NULL;
+static bool g_eth_connected = false;
 
 /** Event handler for Ethernet events */
 static void eth_event_handler(void *arg, esp_event_base_t event_base,
@@ -31,12 +33,15 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base,
         break;
     case ETHERNET_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "Ethernet Link Down");
+        g_eth_connected = false;
         break;
     case ETHERNET_EVENT_START:
         ESP_LOGI(TAG, "Ethernet Started");
+        g_eth_connected = false;
         break;
     case ETHERNET_EVENT_STOP:
         ESP_LOGI(TAG, "Ethernet Stopped");
+        g_eth_connected = false;
         break;
     default:
         break;
@@ -56,6 +61,7 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
     ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
     ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
     ESP_LOGI(TAG, "~~~~~~~~~~~");
+    g_eth_connected = true;
 }
 
 void eth_init(void)
@@ -101,7 +107,7 @@ void eth_init(void)
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
     };
-    ESP_ERROR_CHECK(spi_bus_initialize(CONFIG_NETWORK_ETH_SPI_HOST, &buscfg, 1));
+    ESP_ERROR_CHECK(spi_bus_initialize(CONFIG_NETWORK_ETH_SPI_HOST, &buscfg, SPI_DMA_CH_AUTO));
 #if CONFIG_NETWORK_USE_DM9051
     spi_device_interface_config_t devcfg = {
         .command_bits = 1,
@@ -149,4 +155,211 @@ void eth_init(void)
     ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle)));
     /* start Ethernet driver state machine */
     ESP_ERROR_CHECK(esp_eth_start(eth_handle));
+    g_eth_netif = eth_netif;
 }
+
+void eth_wait_connected(void)
+{
+    ESP_LOGI(TAG, "Ethernet Waiting IP");
+    while (g_eth_connected == false)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    ESP_LOGI(TAG, "Ethernet Got IP Success");
+}
+
+bool eth_config(char* ip, char* mask, char* gw, char* dns1, char* dns2)
+{
+    const char* TAG = "eth_config";
+    if(g_eth_netif == NULL)
+    {
+        return false;
+    }
+    esp_netif_t * esp_netif = g_eth_netif;
+
+    esp_netif_dhcp_status_t status = ESP_NETIF_DHCP_INIT;
+	esp_netif_ip_info_t info = { 0 };
+    info.ip.addr = ipaddr_addr(ip);
+    info.gw.addr = ipaddr_addr(gw);
+    info.netmask.addr = ipaddr_addr(mask);
+
+    esp_err_t err = ESP_OK;
+    err = esp_netif_dhcpc_get_status(esp_netif, &status);
+    if(err)
+    {
+        ESP_LOGE(TAG, "DHCPC Get Status Failed! 0x%04x, %s", err, esp_err_to_name(err));
+        return false;
+    }
+    // stop dhcp
+    err = esp_netif_dhcpc_stop(esp_netif);
+    if(err && err != ESP_ERR_ESP_NETIF_DHCP_ALREADY_STOPPED)
+    {
+        ESP_LOGE(TAG, "DHCPC Stop Failed! 0x%04x, %s", err, esp_err_to_name(err));
+        return err;
+    }
+    // set ip info
+    err = esp_netif_set_ip_info(esp_netif, &info);
+    if(err)
+    {
+        ESP_LOGE(TAG, "Netif Set IP Failed! 0x%04x, %s", err, esp_err_to_name(err));
+        return false;
+    }
+    // if dhcp
+    if(info.ip.addr == 0)
+    {
+        err = esp_netif_dhcpc_start(esp_netif);
+        if(err)
+        {
+            ESP_LOGE(TAG, "DHCPC Start Failed! 0x%04x, %s", err, esp_err_to_name(err));
+            return false;
+        }
+    }
+    // dns server
+    esp_netif_dns_info_t dns = { 0 };
+	dns.ip.type = ESP_IPADDR_TYPE_V4;
+	dns.ip.u_addr.ip4.addr = ipaddr_addr(dns1);
+	if(dns.ip.u_addr.ip4.addr && esp_netif_set_dns_info(esp_netif, ESP_NETIF_DNS_MAIN, &dns) != ESP_OK)
+    {
+    	ESP_LOGE(TAG, "Set Main DNS Failed!");
+    	return false;
+    }
+    
+    dns.ip.u_addr.ip4.addr = ipaddr_addr(dns2);
+    if(dns.ip.u_addr.ip4.addr && esp_netif_set_dns_info(esp_netif, ESP_NETIF_DNS_BACKUP, &dns) != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Set Backup DNS Failed!");
+    	return false;
+    }
+    return true;
+}
+
+bool eth_got_ip(void)
+{
+    return g_eth_connected;
+}
+
+char* eth_get_ip(void)
+{
+    const char* TAG = "eth_get_ip";
+    if(g_eth_netif == NULL)
+    {
+        return NULL;
+    }
+    esp_netif_t * esp_netif = g_eth_netif;
+
+    esp_netif_ip_info_t info = { 0 };
+    esp_err_t err = esp_netif_get_ip_info(esp_netif, &info);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Netif Get IP Failed! 0x%04x, %s", err, esp_err_to_name(err));
+        return NULL;
+    }
+    
+    ip4_addr_t addr = {
+        .addr = info.ip.addr,
+    };
+    static char str[IP4ADDR_STRLEN_MAX];
+    return ip4addr_ntoa_r(&addr, str, IP4ADDR_STRLEN_MAX);
+}
+
+char* eth_get_mask(void)
+{
+    const char* TAG = "eth_get_mask";
+    if(g_eth_netif == NULL)
+    {
+        return NULL;
+    }
+    esp_netif_t * esp_netif = g_eth_netif;
+
+    esp_netif_ip_info_t info = { 0 };
+    esp_err_t err = esp_netif_get_ip_info(esp_netif, &info);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Netif Get IP Failed! 0x%04x, %s", err, esp_err_to_name(err));
+        return NULL;
+    }
+    
+    ip4_addr_t addr = {
+        .addr = info.netmask.addr,
+    };
+    static char str[IP4ADDR_STRLEN_MAX];
+    return ip4addr_ntoa_r(&addr, str, IP4ADDR_STRLEN_MAX);
+}
+
+char* eth_get_gw(void)
+{
+    const char* TAG = "eth_get_gw";
+    if(g_eth_netif == NULL)
+    {
+        return NULL;
+    }
+    esp_netif_t * esp_netif = g_eth_netif;
+
+    esp_netif_ip_info_t info = { 0 };
+    esp_err_t err = esp_netif_get_ip_info(esp_netif, &info);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Netif Get IP Failed! 0x%04x, %s", err, esp_err_to_name(err));
+        return NULL;
+    }
+    
+    ip4_addr_t addr = {
+        .addr = info.gw.addr,
+    };
+    static char str[IP4ADDR_STRLEN_MAX];
+    return ip4addr_ntoa_r(&addr, str, IP4ADDR_STRLEN_MAX);
+}
+
+char* eth_get_dns1(void)
+{
+    const char* TAG = "eth_get_dns1";
+    if(g_eth_netif == NULL)
+    {
+        return NULL;
+    }
+    esp_netif_t * esp_netif = g_eth_netif;
+
+    esp_netif_dns_info_t dns = { 0 };
+    esp_err_t err = esp_netif_get_dns_info(esp_netif, ESP_NETIF_DNS_MAIN, &dns);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Netif Get DNS Info Failed! 0x%04x, %s", err, esp_err_to_name(err));
+        return NULL;
+    }
+
+    ESP_LOGW("DNS Main Servers", IPSTR, IP2STR(&dns.ip.u_addr.ip4));
+
+    ip4_addr_t addr = {
+        .addr = dns.ip.u_addr.ip4.addr,
+    };
+    static char str[IP4ADDR_STRLEN_MAX];
+    return ip4addr_ntoa_r(&addr, str, IP4ADDR_STRLEN_MAX);
+}
+
+char* eth_get_dns2(void)
+{
+    const char* TAG = "eth_get_dns2";
+    if(g_eth_netif == NULL)
+    {
+        return NULL;
+    }
+    esp_netif_t * esp_netif = g_eth_netif;
+
+    esp_netif_dns_info_t dns = { 0 };
+    esp_err_t err = esp_netif_get_dns_info(esp_netif, ESP_NETIF_DNS_BACKUP, &dns);
+    if(err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Netif Get DNS Info Failed! 0x%04x, %s", err, esp_err_to_name(err));
+        return NULL;
+    }
+
+    ESP_LOGW("DNS Backup Servers", IPSTR, IP2STR(&dns.ip.u_addr.ip4));
+
+    ip4_addr_t addr = {
+        .addr = dns.ip.u_addr.ip4.addr,
+    };
+    static char str[IP4ADDR_STRLEN_MAX];
+    return ip4addr_ntoa_r(&addr, str, IP4ADDR_STRLEN_MAX);
+}
+
+
--- eth/eth.h
+++ eth/eth.h
@@ -7,8 +7,18 @@ extern "C" {
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 void eth_init(void);
+void eth_wait_connected(void);
+bool eth_config(char* ip, char* mask, char* gw, char* dns1, char* dns2);
+bool eth_got_ip(void);
+char* eth_get_ip(void);
+char* eth_get_mask(void);
+char* eth_get_gw(void);
+char* eth_get_dns1(void);
+char* eth_get_dns2(void);
+
 
 #if __cplusplus
 }
